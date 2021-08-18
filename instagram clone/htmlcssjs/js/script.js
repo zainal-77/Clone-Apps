c0fb70b2db12a932af42f094ecdbdcfe84b55ad7
zainal-77 <zainal.726290@gmail.com>
2021-08-18 16:42:50 +0700

    commits

---
+++ instagram clone/htmlcssjs/js/script.js
@@ -0,0 +1,10 @@
+function menuLike(){
+	var element =document.getElementById('menu-like');
+	element.classList.toggle('showMenuLike');
+}
+
+function menuProfile() {
+	// body...
+	var element= document.getElementById('menu-profile');
+	element.classList.toggle("showMenuProfile");
+}
---
+++ instagram clone/instgrm-clone/resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+          <img src="https://instagram.fsub8-1.fna.fbcdn.net/v/t51.2885-19/s150x150/89259596_825092881334678_5719615060549042176_n.jpg?_nc_ht=instagram.fsub8-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=b_5JQs4GHRkAX83hQFq&oh=470054d4aec62d1fb6ba1455f3959d70&oe=5FBFBC3F" alt="dd">  
+        </div>
+        <div class="col-9">
+
+        </div>
+    </div>
+</div>
+@endsection
